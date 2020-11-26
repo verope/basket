25ef9ad1ee72747f4d9535cbf188c8e86d97c1bf
verope <vero.peskova@gmail.com>
2020-11-26 21:52:07 +0100

    Added hompage + styles

--- apps/homepage.py
+++ apps/homepage.py
@@ -3,14 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
-
 sys.path.append("..") # Adds higher directory to python modules path.
 
-from graphs.itesco_weighted_evo import graph as graph1
-from graphs.itesco_main_category import graph as graph2
-from graphs.itesco_sub_category2 import graph as graph3
-from graphs.itesco_product import graph as graph4
 from app import app
 from layout.navbar import navbar
 
@@ -18,6 +12,20 @@ from layout.navbar import navbar
 layout = html.Div(
     [
         navbar,
+        html.Div([
+            html.H1('Analýza spotřebního koše z online potravinových e-shopů', className="homepage__hero-heading"),
+            html.P('paragrah', className="homepage__hero-paragraph"),
+            html.A('CTA - SHOWCASE', className="homepage__hero-showcase-link", href="/showcase"),
+        ], className="homepage__hero"),
+        html.Hr(className="homepage__divider"),
+        html.H2('Podívejte se na interaktivní dashboardy pro:', className="homepage__box-heading"),
+        html.Div([
+            html.A(className="homepage__box homepage__box--itesco", href="/itesco"),
+            html.A(className="homepage__box homepage__box--rohlik", href="#rohlik"),
+            html.A(className="homepage__box homepage__box--kosik", href="#kosik"),
+        ], className="homepage__box-container"),
+        html.Hr(className="homepage__divider"),
+        # o nas 
     ],
-    # className="layout-content__container"
+    className="layout-content__container"
 )
