63023a370fa1b6478a6333294ad139aa7296d686
verope <vero.peskova@gmail.com>
2020-11-27 01:10:29 +0100

    changed server settings for deployment

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ rc_itesco_main_cat_graph_agg(app)
 rc_itesco_product_graph_dropdown(app)
 
 if __name__ == '__main__':
-    app.run_server(debug=True,host='127.0.0.1',port = 8050)
-    # app.run_server(debug=False)
+    # app.run_server(debug=True,host='127.0.0.1',port = 8050)
+    app.run_server(debug=False)
     # server debug does not work -> fix!
